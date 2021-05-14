b0fd0e10e4165cbad53afcb04804ea650ca26a6f
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-14 00:00:37 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,7 +3,7 @@ import { MissingParamError } from '../errors/missing-param-error'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController() // sysytem under test
+    const sut = new SignUpController() // system under test
 
     const httpRequest = {
       body: {
@@ -20,7 +20,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if no email is provided', () => {
-    const sut = new SignUpController() // sysytem under test
+    const sut = new SignUpController() // system under test
 
     const httpRequest = {
       body: {
@@ -33,6 +33,23 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email')) // toBe is not used in this case bacause when the comparison is objects, it also checks the reference, thus it will fail.
+    // toEqual checks only the value
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController() // system under test
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@test.com',
+        name: 'any_name',
+        passwordConfirmation: 'any_password'
+
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password')) // toBe is not used in this case bacause when the comparison is objects, it also checks the reference, thus it will fail.
     // toEqual checks only the value
   })
 })
