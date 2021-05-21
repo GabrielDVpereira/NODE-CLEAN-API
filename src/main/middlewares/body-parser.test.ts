9f4cd785f1c3ef47469082adef00a60f65af49a9
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-21 07:46:29 -0300

    test: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,16 @@
+// it has .test because it's an integration test, we have a script in package.json only to run integration tests
+
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body Parser Middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Gabriel' })
+      .expect({ name: 'Gabriel' })
+  })
+})
