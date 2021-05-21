45a5bcef14fae0690a2fea45dca45fc0dd460e2b
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-21 07:13:52 -0300

    refactor: clear db between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -9,6 +9,10 @@ describe('Account Mongo Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({}) // cleaning the collection before each test
+  })
 
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
