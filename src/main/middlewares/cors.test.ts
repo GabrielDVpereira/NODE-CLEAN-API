594baa4f2dda8c32a11fc82a9cec78a1ec6cd382
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-21 07:58:24 -0300

    refactor: remove unecessary use of req

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -6,7 +6,7 @@ import app from '../config/app'
 describe('Cors Middleware', () => {
   test('should enable CORS', async () => {
     app.post('/test_cors', (req, res) => {
-      res.send(req.body)
+      res.send()
     })
     await request(app)
       .get('/test_body_parser')
