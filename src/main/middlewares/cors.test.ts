160e466314813ce47440eba4ceacdae2f0ec7ca1
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-21 07:55:48 -0300

    test: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,17 @@
+// it has .test because it's an integration test, we have a script in package.json only to run integration tests
+
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Cors Middleware', () => {
+  test('should enable CORS', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .get('/test_body_parser')
+      .expect('access-control-allow-origin', '*') // we expect we can acess requests from any server
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
