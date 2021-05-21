705415a3506793e63700f834c4bbe9c55b42498e
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-21 07:55:30 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,7 @@
 import { Express } from 'express'
 import { bodyparser } from '../middlewares/body-parser'
+import { cors } from '../middlewares/cors'
 export default (app: Express): void => {
   app.use(bodyparser)
+  app.use(cors)
 }
