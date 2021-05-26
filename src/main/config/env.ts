61bbf13493f0f3b027639a97180c7c520a2a1208
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-25 23:10:48 -0300

    refactor: ensure express routes adapter return error message instead of error name

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clea-_node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean_node-api',
   port: process.env.PORT || 5050
 }
