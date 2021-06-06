15353a4082468511d768a98c6bdcc142ecc81736
Gabriel Davi <gdavi.gd@gmail.com>
2021-06-06 15:29:07 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean_node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || '3fc854f6376585d5b379a5c403ad81c7'
 }
