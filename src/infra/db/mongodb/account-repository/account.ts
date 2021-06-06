11a206d39df4246b9b38088355635f5ac37fba90
Gabriel Davi <gdavi.gd@gmail.com>
2021-06-06 14:11:15 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,10 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountColletion = await MongoHelper.getCollection('accounts')
     const result = await accountColletion.insertOne(accountData) // returns the result of the operation
@@ -11,5 +12,13 @@ export class AccountMongoRepository implements AddAccountRepository {
     const account = result.ops[0] // the result is in the ops array. Since we inserted one, theres only one position
 
     return MongoHelper.map(account)
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountColletion = await MongoHelper.getCollection('accounts')
+    const account = await accountColletion.findOne({ email })
+    if (account) {
+      return MongoHelper.map(account)
+    }
   }
 }
