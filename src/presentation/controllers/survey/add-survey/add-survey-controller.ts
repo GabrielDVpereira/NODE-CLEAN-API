9bca4cbc7574336688f5f37bea8bb701d6ac1a86
Gabriel Davi <gdavi.gd@gmail.com>
2021-06-24 23:38:11 -0300

    feat: ensure add survey controller return 500 if add survey usecase throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,5 +1,5 @@
 import { AddSurvey } from '../../../../domain/usecases/add-survey'
-import { badRequest } from '../../../helpers/http/http-helpers'
+import { badRequest, serverError } from '../../../helpers/http/http-helpers'
 import { Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
@@ -10,13 +10,17 @@ export class AddSurveyController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
-    }
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
 
-    const { question, answers } = httpRequest.body
-    await this.addSurvey.add({ question, answers })
-    return Promise.resolve(null)
+      const { question, answers } = httpRequest.body
+      await this.addSurvey.add({ question, answers })
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
