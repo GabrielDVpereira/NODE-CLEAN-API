5d477ee07fbcf3f9d7849d97d76cddfa898930a3
Gabriel Davi <gdavi.gd@gmail.com>
2021-06-27 20:56:19 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,19 @@
+
+import { Validation } from '../../../../presentation/protocols/validation'
+import { ValidationComposite, RequiredFieldValidation } from '../../../../validation/validators'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+
+jest.mock('../../../../validation/validators/validation-composite') // we mock this path, so when anyone calls this, will get a mocked result
+
+describe('AddSurveyValidation factory', () => {
+  test('Should call validation composite with all validations', () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = []
+
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
---
+++ src/main/routes/sourvey-routes.test.ts
@@ -0,0 +1,38 @@
+// it has .test because it's an integration test, we have a script in package.json only to run integration tests
+
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let surveyCollection: Collection
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL) // jest-mongodb sets a db url for us in the env
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({}) // cleaning the collection before each test
+  })
+  describe('POST /surveys', () => {
+    test('should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
+    })
+  })
+})
