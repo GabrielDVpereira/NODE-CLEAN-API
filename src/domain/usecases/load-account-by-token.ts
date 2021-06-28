4f9f70577320cbd4fd8874c530d3fb7692965b1d
Gabriel Davi <gdavi.gd@gmail.com>
2021-06-27 21:41:24 -0300

    feat: ensure auth middleware calls load account by token with correct token

---
+++ src/domain/usecases/load-account-by-token.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../models/account'
+
+export interface LoadAccountByToken {
+  load: (accessToken: string, role?: string) => Promise<AccountModel> // AccountModel is database related, so we dont use it inside domain
+}
