127bddf051a2f36108aebbbd2912faad677a1550
Gabriel Davi <gdavi.gd@gmail.com>
2021-06-29 23:00:29 -0300

    test: ensure add survey return 204 if valid access token is provided

--- src/main/routes/sourvey-routes.test.ts
+++ src/main/routes/sourvey-routes.test.ts
@@ -4,8 +4,11 @@ import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL) // jest-mongodb sets a db url for us in the env
@@ -17,6 +20,9 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({}) // cleaning the collection before each test
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({}) // cleaning the collection before each tes
   })
   describe('POST /surveys', () => {
     test('should return 403 on add survey without access token', async () => {
@@ -33,6 +39,41 @@ describe('Survey Routes', () => {
           }]
         })
         .expect(403)
+    })
+    test('should return 204 on add survey with valid access token', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Gabriel',
+        email: 'Gabriel.davi.99@gmail.com',
+        password: '123',
+        role: 'admin'
+
+      })
+      const id = res.ops[0]._id
+
+      const accessToken = sign({ id }, env.jwtSecret)
+
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
     })
   })
 })
