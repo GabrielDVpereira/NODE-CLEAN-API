a8acb98e853fbc2d9f24beedee0fdec8aa1e352d
gabrieldvpereira <gdavi.gd@gmail.com>
2021-07-29 21:18:28 -0300

    fix: allows api to run locally or with docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean_node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean_node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '3fc854f6376585d5b379a5c403ad81c7'
 }
